9e47879e8fecc4db00399a452b7154a0264d22c6
ryo-arakawa <arkw.bump@gmail.com>
2022-01-10 18:33:11 +0900

    オブジェクトの型の付け方について

--- index.ts
+++ index.ts
@@ -1,4 +1,3 @@
-let hasValue: boolean = true;
 let count: number = 10;
 let float: number = 3.14;
 let negative: number = -0.12;
@@ -7,3 +6,24 @@ let double: string = 'hello';
 let back: string = `hello`;
 
 // vscodeも全てtypescriptで書かれている
+
+// 基本的には型推論でかく
+// 型推論ができない時のみ型注釈でかく
+
+
+let hasValue = true;
+
+let hello; //any なんでもok
+
+// オブジェクトに型をつける
+
+const person = {
+    name:{
+        first: 'Jack',
+        last: 'Smith'
+    },
+        age: 21
+}
+console.log(person.gender);  //ないものはちゃんと弾かれる
+
+// 配列
